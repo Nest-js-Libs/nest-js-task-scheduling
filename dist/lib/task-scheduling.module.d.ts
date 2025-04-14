6283c46ed5411a799edbecd74a3303776d0b5405
yasnielfajardo <yasnielfajardoegues1408@gmail.com>
2025-04-13 21:38:06 -0300

    refactor

--- dist/lib/task-scheduling.module.d.ts
+++
@@ -1,7 +0,0 @@
-import { DynamicModule } from '@nestjs/common';
-export interface TaskSchedulingModuleOptions {
-    queues: string[];
-}
-export declare class TaskSchedulingModule {
-    static forRoot(options: TaskSchedulingModuleOptions): DynamicModule;
-}
